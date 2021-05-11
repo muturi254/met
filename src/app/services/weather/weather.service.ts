bfe2bf2176fb4996559057c642f4d7df961c2e14
muturi254 <petermuturi74@gmail.com>
2021-05-12 01:43:56 +0300

    add metric unit to response

--- src/app/services/weather/weather.service.ts
+++ src/app/services/weather/weather.service.ts
@@ -9,11 +9,12 @@ import { environment } from 'src/environments/environment.prod';
 })
 export class WeatherService {
 
-  url = `${environment.BASE_URL}nairobi&&appid=${environment.API_KEY}`;
+  api_url = `${environment.BASE_URL}nairobi&&appid=${environment.API_KEY}&&units=metric`;
+  
 
   constructor(private http: HttpClient) { }
 
   getCurrentWeather(): Observable<ICurrentWeather> {
-    return this.http.get<ICurrentWeather>(this.url);
+    return this.http.get<ICurrentWeather>(this.api_url);
   }
 }
