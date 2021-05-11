d186394f06cf683803b4fd00baf92c0e88226d9b
muturi254 <petermuturi74@gmail.com>
2021-05-12 00:31:12 +0300

    add service to fetch api and return current weather

--- src/app/services/weather/weather.service.ts
+++ src/app/services/weather/weather.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { Observable } from 'rxjs';
+import { ICurrentWeather } from 'src/app/interfaces/current';
+import { environment } from 'src/environments/environment.prod';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherService {
 
-  constructor() { }
+  url = `${environment.BASE_URL}nairobi&&appid=${environment.API_KEY}`;
+
+  constructor(private http: HttpClient) { }
+
+  getCurrentWeather(): Observable<ICurrentWeather> {
+    return this.http.get<ICurrentWeather>(this.url);
+  }
 }
