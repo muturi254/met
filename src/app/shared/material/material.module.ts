8643f1090169aac77b7a9eb716c6ed555d7b1db6
muturi254 <petermuturi74@gmail.com>
2021-05-11 18:43:51 +0300

    add project folder set up

---
+++ src/app/shared/material/material.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+
+
+const materialComponents = [
+  MatButtonModule
+]
+
+@NgModule({
+  imports: [materialComponents],
+  exports: [materialComponents],
+})
+export class MaterialModule { }
